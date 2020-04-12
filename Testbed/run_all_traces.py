834ac973c320310df9c8046802d78f561508d8f3
William Ye <willwei79@gmail.com>
2020-04-12 11:56:32 -0400

    tracing the mahimahi trace stuff

--- Testbed/run_all_traces.py
+++ Testbed/run_all_traces.py
@@ -15,10 +15,14 @@ os.system('sudo sysctl -w net.ipv4.ip_forward=1')
 ip_data = json.loads(urllib.urlopen("http://ip.jsontest.com/").read())
 ip = str(ip_data['ip'])
 
+print ip
+
 # LIST ALGOS HERE TO RUN OTHER TRACES
 ABR_ALGO = "RL"
 PROCESS_ID = 0
 command_rl = 'python run_traces.py ' + TRACE_PATH + ' ' + ABR_ALGO + ' ' + str(PROCESS_ID) + ' ' + ip
+
+print command_rl
 
 # Processes
 process_rl = subprocess.Popen(command_rl, stdout=subprocess.PIPE, shell=True)
