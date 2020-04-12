90b817ffa6f9c581d62b922cf139314c5b4aa700
root <root@instance-1.us-west1-a.c.deeplearning-260510.internal>
2020-04-12 18:34:24 +0000

    added rl files

---
+++ rl/test/run_testing.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+python get_video_sizes.py
+python rl_no_training.py
+python plot_results.py
---
+++ rl/train/run_training.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+python get_video_sizes.py
+python multi_agent.py
